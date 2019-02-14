e6cd67f91b888ee0c0993968e4570be8b0c43423
Sumit Kumar <sumit.sh25@yahoo.com>
2019-02-14 20:01:41 +0530

    added more lessons

---
+++ 2_1_project_files/js_sandbox/app.js
@@ -0,0 +1,12 @@
+
+console.log('Hello')
+console.log(12)
+console.log(true)
+console.log([1,2,3,4])
+console.log({name:'Sumit', age:22})
+console.table({name:'Sumit', age:22})
+console.time('Hello')
+    for(var i =100; i<=1000; i++){
+        console.log(i*i);
+    }
+console.timeEnd('Hello')
